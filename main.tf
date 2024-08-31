9578cd31bf7bba6563c1ea987ceb91ea2f6fa071
suyashay <155423372+suyashay@users.noreply.github.com>
2024-08-31 14:49:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,3 +31,28 @@ resource "aws_subnet" "web-subnet-1" {
     Name = "Web-1a"
   }
 }
+
+resource "aws_subnet" "web-subnet-2" {
+  vpc_id                  = aws_vpc.my-vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+ 
+  tags = {
+    Name = "Web-2a"
+  }
+}
+
+
+# Create Application Private Subnet
+resource "aws_subnet" "application-subnet-1" {
+  vpc_id                  = aws_vpc.my-vpc.id
+  cidr_block              = "10.0.11.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "Application-1a"
+  }
+}
+
