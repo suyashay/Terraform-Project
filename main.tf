b7b2bfebd223ce4353ec57f28e7b706fceb80b18
root <root@ip-172-31-93-183.ec2.internal>
2024-08-31 09:01:30 +0000

    commit-3

--- main.tf
+++ main.tf
@@ -19,3 +19,15 @@ resource "aws_vpc" "my-vpc" {
     Name = "my-VPC"
   }
 }
+
+# Create Web Public Subnet
+resource "aws_subnet" "web-subnet-1" {
+  vpc_id                  = aws_vpc.my-vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Web-1a"
+  }
+}
