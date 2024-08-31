e46c71df02d069dd54b94bd56e00e160b350f36b
suyashay <155423372+suyashay@users.noreply.github.com>
2024-08-31 18:58:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 
 # Create a VPC
 resource "aws_vpc" "my-vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = "10.48.0.0/16"
   tags = {
     Name = "my-VPC"
   }
@@ -23,7 +23,7 @@ resource "aws_vpc" "my-vpc" {
 # Create Web Public Subnet
 resource "aws_subnet" "web-subnet-1" {
   vpc_id                  = aws_vpc.my-vpc.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = "10.48.1.0/24"
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 
@@ -34,7 +34,7 @@ resource "aws_subnet" "web-subnet-1" {
 
 resource "aws_subnet" "web-subnet-2" {
   vpc_id                  = aws_vpc.my-vpc.id
-  cidr_block              = "10.0.2.0/24"
+  cidr_block              = "10.48.2.0/24"
   availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
  
@@ -47,7 +47,7 @@ resource "aws_subnet" "web-subnet-2" {
 # Create Application Private Subnet
 resource "aws_subnet" "application-subnet-1" {
   vpc_id                  = aws_vpc.my-vpc.id
-  cidr_block              = "10.0.11.0/24"
+  cidr_block              = "10.48.10.0/24"
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = false
 
@@ -56,3 +56,95 @@ resource "aws_subnet" "application-subnet-1" {
   }
 }
 
+resource "aws_subnet" "application-subnet-2" {
+  vpc_id                  = aws_vpc.my-vpc.id
+  cidr_block              = "10.48.20.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "Application-2b"
+  }
+}
+
+# Create Database Private Subnet
+resource "aws_subnet" "database-subnet-1" {
+  vpc_id            = aws_vpc.my-vpc.id
+  cidr_block        = "10.48.30.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "Database-1a"
+  }
+}
+
+resource "aws_subnet" "database-subnet-2" {
+  vpc_id            = aws_vpc.my-vpc.id
+  cidr_block        = "10.48.40.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "Database-2b"
+  }
+}
+
+# Create Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.my-vpc.id
+
+  tags = {
+    Name = "my-IGW"
+  }
+}
+
+# Create Web layer route table
+resource "aws_route_table" "web-rt" {
+  vpc_id = aws_vpc.my-vpc.id
+
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "WebRT"
+  }
+}
+
+# Create Web Subnet association with Web route table
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.web-subnet-1.id
+  route_table_id = aws_route_table.web-rt.id
+}
+
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.web-subnet-2.id
+  route_table_id = aws_route_table.web-rt.id
+}
+
+# Create EC2 Instance
+resource "aws_instance" "webserver1" {
+  ami                    = "ami-0d5eff06f840b45e9"
+  instance_type          = "t2.micro"
+  availability_zone      = "us-east-1a"
+  key_name               = "jrb"
+  vpc_security_group_ids = [aws_security_group.webserver-sg.id]
+  subnet_id              = aws_subnet.web-subnet-1.id
+  user_data              = file("install_apache.sh")
+
+  tags = {
+    Name = "Web Server"
+  }
+
+  provisioner "file" {
+    source      = "/var/lib/jenkins/workspace/terraformpipeline/index.html"
+    destination = "/var/www/html/index.html"
+
+  connection {
+      type        = "ssh"
+      host        = self.public_ip
+      user        = "ec2-user"
+    }
+  }
+}
